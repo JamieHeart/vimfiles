320ab9f844ac934478a9f0e889ffba7b866b2458
Jamie Hartman <jamie.hartman@act-on.net>
2018-12-14 22:54:26 -0800

    created install bash script to install this .vimrc into the users home dir

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+VIMRC="${HOME}/.vimrc"
+VIM_DIR="${HOME}/.vim"
+AUTOLOAD="${VIM_DIR}/autoload"
+BUNDLE="${VIM_DIR}/bundle"
+#check for existing ~/.vimrc file
+if [ -e "${VIMRC}" ] ; then
+		echo "you already have a ~/.vimrc file"
+else
+		#	install .vimrc
+		ln -s ${PWD}/vimrc ${VIMRC}
+fi
+
+#	nope, use the native package manager
+exit 0
+#	Install pathogen
+if [ ! -e "${AUTOLOAD}" ] ; then
+		mkdir -p "${AUTOLOAD}"
+fi
+
+if [ ! -e "${BUNDLE}" ] ; then
+		mkdir -p "${BUNDLE}"
+fi
+
+if [ -e "${AUTOLOAD}/pathogen.vim" ] ; then
+		echo "pathogen.vim already installed"
+else
+	ln -s ${PWD}/vim/autoload/pathogen.vim ${AUTOLOAD}/pathogen.vim
+fi
